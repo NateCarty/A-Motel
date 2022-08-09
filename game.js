bd0319cdedc74ebfd458d20a8e8028eac0d3e4e3
Nate Carty <natecartylife@gmail.com>
2022-08-09 17:34:18 -0400

    Added delay function to button

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 const btn = document.getElementById('button')
 
-
+var buttonDelay = 1000;
 btn.addEventListener("click", () => {
     console.log("clicked")
-    btn.disabled = true;
+    btn.disabled = true
+    setTimeout(function() {
+        btn.disabled = false
+    }, buttonDelay);
 })
